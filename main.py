bb7967dd7cb5bd096b5e03537e3dcc8c5a0a8f07
Arthur Negrão <arthurnegrao10s@gmail.com>
2024-02-21 23:38:19 -0300

    Deu Velha
    Agora aceita perfeitamente quando dá velha

--- main.py
+++ main.py
@@ -12,28 +12,29 @@ while loop:
         print('Player: O\nMáquina: X')
     else:
         print('Player: X\nMáquina: O')
-    volta = 0
+    turno = 0 # Contador para minimizará computação dos laços desnecessariamente e identificará se deu velha quando alcançar 9 jogadas
     if comeca_jogando: # Se o player começar
         while True: # Roda enquanto a partida tiver valor de False
-            volta += 1 # Contador para minimizara computação dos laços desnecessariamente
             jogo.printar_tabuleiro()
-            if volta >= 5: # Define se deu velha
-                resultado = jogo.ganhou(opcao='X')
-                if not resultado:
-                    jogo.printar_tabuleiro()
-                    print('Deu velha')
-                    break
             tratamento_erro = True
             while tratamento_erro:
                 tratamento_erro = jogo.escolha_pessoa() # Realiza a jogada do player e trata os erros
-            if volta >= 2:
+            turno += 1 # Contador para minimizará computação dos laços desnecessariamente
+            if turno >= 4:
                 resultado = jogo.ganhou(opcao='O')
                 if resultado:
                     jogo.printar_tabuleiro()
                     print('O player ganhou')
                     break
+            if turno == 9: # Define se deu velha
+                resultado = jogo.ganhou(opcao='X')
+                if not resultado:
+                    jogo.printar_tabuleiro()
+                    print('Deu velha')
+                    break
             jogo.jogar_pc() # Realiza a jogada da máquina
-            if volta >= 2:
+            turno += 1
+            if turno >= 4:
                 resultado = jogo.ganhou(opcao='X')
                 if resultado:
                     jogo.printar_tabuleiro()
@@ -41,25 +42,26 @@ while loop:
                     break
     else: # A máquina começa
         while True: # Roda enquanto a partida tiver valor de False
-            volta += 1 # Contador para minimizara computação dos laços desnecessariamente
-            if volta >= 5: # Vai definir se Deu Velha
-                resultado = jogo.ganhou(opcao='X')
-                if not resultado:
-                    jogo.printar_tabuleiro()
-                    print('Deu velha')
-                    break
             jogo.jogar_pc() # Realiza a jogada da máquina
+            turno += 1 # Contador para minimizara computação dos laços desnecessariamente
             jogo.printar_tabuleiro()
-            if volta >= 2:
+            if turno >= 4:
                 resultado = jogo.ganhou(opcao='O')
                 if resultado:
                     jogo.printar_tabuleiro()
                     print('A máquina ganhou')
                     break
             tratamento_erro = True
+            if turno == 9: # Vai definir se Deu Velha
+                resultado = jogo.ganhou(opcao='X')
+                if not resultado:
+                    jogo.printar_tabuleiro()
+                    print('Deu velha')
+                    break
             while tratamento_erro:
                 tratamento_erro = jogo.escolha_pessoa() # Realiza a jogada do player e trata os erros
-            if volta >= 2:
+            turno += 1
+            if turno >= 4:
                 resultado = jogo.ganhou(opcao='X')
                 if resultado:
                     jogo.printar_tabuleiro()
